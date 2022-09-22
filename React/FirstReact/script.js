ecd06d978485a4e963d1640d687f46c2d7c7341b
Mann Patel <mannpatel3118@gmail.com>
2022-09-22 15:47:37 +0530

    first react code

---
+++ React/FirstReact/script.js
@@ -0,0 +1,14 @@
+function MainContent() {
+    return (
+        <div>
+
+            <h1>I'm learning React!</h1>
+            <p><i>Code - Design - Rest</i></p>
+        </div>
+    )
+}
+ReactDOM.render(
+    <div>
+        <MainContent />
+    </div>,document.getElementById("root")
+)
