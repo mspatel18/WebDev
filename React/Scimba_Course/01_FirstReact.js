bb7da0a8223e01650d74837352241be89dfe13cf
Mann Patel <mannpatel3118@gmail.com>
2022-09-24 23:32:21 +0530

    2409

--- React/Scimba_Course/01_FirstReact.js
+++ React/Scimba_Course/01_FirstReact.js
@@ -1,10 +1,9 @@
 function MainContent() {
     return (
-        <div>
-
+        <>
             <h1>I'm learning React!</h1>
-            <p><i>Code - Design - Rest</i></p>
-        </div>
+            <p><i>//Code - Design - Rest//</i></p>
+        </>
     )
 }
 ReactDOM.render(
