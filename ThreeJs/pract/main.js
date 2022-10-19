49e14a851905f865dfc47dcad6ee64b3af382163
Mann Patel <mannpatel3118@gmail.com>
2022-10-19 21:41:23 +0530

    cube world

--- ThreeJs/pract/main.js
+++ ThreeJs/pract/main.js
@@ -19,7 +19,7 @@ document.body.appendChild( renderer.domElement );
 
 const loader = new GLTFLoader();
 
-loader.load( '/threejs-test.glb', function ( gltf ) {
+loader.load( '/gogglepayQRModel.glb', function ( gltf ) {
 
 	scene.add( gltf.scene );
 
@@ -28,30 +28,45 @@ loader.load( '/threejs-test.glb', function ( gltf ) {
 	console.error( error );
 
 } );
-
+function addCubes() {
+	const geometry = new THREE.BoxGeometry(5, 5, 5)
+	const material = new THREE.MeshStandardMaterial({ color: 0xffffff })
+	const cube = new THREE.Mesh(geometry, material)
+	const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(1000))
+	cube.position.set(x, y, z)
+	scene.add(cube)
+  }
+  Array(1000).fill().forEach(addCubes)
 // const geometry = new THREE.BoxGeometry( 5, 5, 5 );
 // const material = new THREE.MeshStandardMaterial( { color: 0xffffff } );
 // const polyhedron = new THREE.Mesh( geometry, material );
 // scene.add( polyhedron );
 
 const ambientlight = new THREE.AmbientLight( 0x404040 ); // soft white light
-const light = new THREE.PointLight( 0x404040, 50, 100 );
-light.position.set( 20, 20,20 );
-scene.add( ambientlight, light );
+const light1 = new THREE.PointLight( 0x404040, 200, 100 );
+light1.position.set( 20, 20,20 );
+const light2 = new THREE.PointLight( 0x404040, 200, 100 );
+light2.position.set( -20, -20,-20 );
+scene.add( ambientlight, light1,light2 );
 
 camera.position.z = 5;
+// camera.position.x = 5;
+// camera.position.y = 5;
 // camera.rotation.x = 1;
 
-const lightHelper = new THREE.PointLightHelper( light );
+const lightHelper = new THREE.PointLightHelper( light1 );
 const gridHelper = new THREE.GridHelper( 200, 50 );
 // const cameraHelper = new THREE.CameraHelper( camera );
 scene.add( lightHelper, gridHelper );
-// const controls = new OrbitControls( camera, renderer.domElement );
+const controls = new OrbitControls( camera, renderer.domElement );
 
 function animate() {
 	requestAnimationFrame( animate );
-	camera.position.x += 0.1;
-//   controls.update();
+	// camera.rotateOnAxis(new THREE.Vector3(0,1,0),0.01);
+	// camera.rotation.z += 0.1;
+	camera.position.y += 0.1;
+	camera.position.z += 0.1;
+  controls.update();
 
 	renderer.render( scene, camera );
 
