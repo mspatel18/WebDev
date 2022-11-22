15d8935ccd80655b22f9f48a6e7e8b76e2de86af
Mann Patel <mannpatel3118@gmail.com>
2022-11-22 22:25:12 +0530

    create basic scene

--- ThreeJs-Journey/17-haunted-house/src/script.js
+++ ThreeJs-Journey/17-haunted-house/src/script.js
@@ -23,8 +23,58 @@ const textureLoader = new THREE.TextureLoader()
 /**
  * House
  */
+//Group
+const house = new THREE.Group()
+scene.add(house) 
+
+//Walls
+const walls = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(4,2.5,4),
+    new THREE.MeshStandardMaterial({color:'#ac8e82' })
+)
+walls.position.y=2.5/2;
+house.add(walls)
+
+//Roof
+const roof = new THREE.Mesh(
+    new THREE.ConeBufferGeometry(3.5,1,4),
+    new THREE.MeshStandardMaterial({color:'#b35f45'})
+)
+roof.position.y = 2.5+0.5
+roof.rotation.y = Math.PI/4
+house.add(roof)
+
+//Door
+const door = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(2,2),
+    new THREE.MeshStandardMaterial({color:'#aa7b7b'})
+)
+door.position.z=2 + 0.001
+door.position.y=1
+house.add(door)
+
+//Bushes
+const bushGeometry = new THREE.SphereBufferGeometry(1,16,16)
+const bushMaterial = new THREE.MeshStandardMaterial({color:'#89c854'})
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush1.scale.set(0.5,0.5,0.5)
+bush1.position.set(0.8,0.25,2,2)
 
 
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush2.scale.set(0.25,0.25,0.25)
+bush2.position.set(1.4,0.1,2.1)
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush3.scale.set(0.4,0.4,0.4)
+bush3.position.set(-0.8,0.1,2.1)
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush4.scale.set(0.15,0.15,0.15)
+bush4.position.set(-1,0.05,2.6)
+
+house.add(bush1,bush2,bush3,bush4)
 // Floor
 const floor = new THREE.Mesh(
     new THREE.PlaneGeometry(20, 20),
