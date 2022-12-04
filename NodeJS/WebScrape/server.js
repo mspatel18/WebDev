d404850925f81f0117f5b080ec8707646df76b0b
Mann Patel <mannpatel3118@gmail.com>
2022-12-05 03:03:15 +0530

    nekobin scrape

--- NodeJS/WebScrape/server.js
+++ NodeJS/WebScrape/server.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 (async () => {
   // Launch a headless Chrome browser
   const browser = await puppeteer.launch();
@@ -8,17 +8,30 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
 
   // Navigate to the website
-  await page.goto('https://zoro.to/home');
+  await page.goto('https://nekobin.com/');
 
   // Select the element with the ID "main-content"
-  const element = await page.$('#anime-trending');
+  const input = await page.waitForSelector('.CodeMirror-line');
+  await input.type('Hello, asdfsafworld!');
 
+  const element = await page.$('.CodeMirror-line');
   // Extract the text of the selected element
   const text = await element.evaluate((el) => el.textContent);
-  const newtext = text.replace(/\s+/g, " ");
+  const element2 = await page.$('.actions');
+  // Extract the text of the selected element
+  const text2 = await element2.evaluate((el) => el.innerHTML);
+  console.log(text2);
+  // const newtext = text.replace(/\s+/g, " ");
   // Print the text to the console
-  console.log(newtext);
-
+  // console.log(newtext);
+  const button = await page.waitForSelector('#save');
+  await button.click();
+  // const newpage = await browser.newPage();
+  await sleep(10000);
+  // console.log(page.url());
+  const url = await page.$('#url');
+  const urltext = await url.evaluate((el) => el.textContent);
+  console.log(urltext);
   // Close the browser
   await browser.close();
 })();
