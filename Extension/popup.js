5b13c879781cd1475dae848fca8821feafb71295
Mann Patel <mannpatel3118@gmail.com>
2023-01-08 14:53:09 +0530

    ✅ fixes

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,35 +1,67 @@
-var inputField = document.getElementById('inputID');
-// var downloadButton = document.getElementById('downloadButton');
-function getInputText() {
-  // Select the input element
-  const inputElement = document.getElementById('inputID');
-
-  // Get the value of the input element
-  const inputText = inputElement.value;
-
-  // Return the input text
-  return inputText;
+async function getPageURL() {
+  const tabs = await new Promise((resolve) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      resolve(tabs);
+    });
+  });
+  const url = tabs[0].url;
+  console.log("The URL of the active tab is: " + url);
+  return url;
 }
-// Listen for clicks on the save button
 
-console.log(document.getElementById('output'))
-inputField.addEventListener('change', function() {
-  console.log(getInputText());
-  function displaylink(link){
-    // console.log(document.getElementById('output'))
-    chrome.tabs.create({url: link});
+(async () => {
+  let pageURL = await getPageURL();
+  console.log(pageURL);
+  function getYouTubeVideoId(url) {
+    // Parse the URL into its component parts
+    var parsedUrl = new URL(url);
+    // Get the value of the `v` parameter
+    var videoId = parsedUrl.searchParams.get("v");
+    // If the `vi` parameter is not present, check for the last part of the pathname
+    if (!videoId) {
+      var pathnameParts = parsedUrl.pathname.split("/");
+      videoId = pathnameParts[pathnameParts.length - 1];
+    }
+    if (!videoId) {
+      // Split the hostname by the `.` character
+      var hostnameParts = parsedUrl.hostname.split(".");
+      // Check if the hostname starts with `youtu`
+      if (hostnameParts[0] === "youtu") {
+        // The last part of the hostname is the video ID
+        videoId = hostnameParts[hostnameParts.length - 1];
+      }
+    }
+    return videoId;
   }
+  let inputElement = document.getElementById("input");
+  inputElement.addEventListener("change", (event) => {
+    pageURL = event.target.value;
+    console.log(pageURL + " " + getYouTubeVideoId(pageURL));
+    fetch(`https://youtube-mp3-download1.p.rapidapi.com/dl?id=${getYouTubeVideoId(pageURL)}`, options)
+      .then((response) => response.json())
+      .then((response) => displaylink(response.link))
+      .catch((err) => console.error(err));
+      
+  });
+  function displaylink(url) {
+    console.log('inside display link')
+    if(url != undefined){
+      document.getElementById('desc').innerHTML = "Click the link below to download the mp3 file";
+      document.getElementById('link').innerHTML = "Download link";
+      document.getElementById('link').setAttribute('href',url);
+    }else{
+      document.getElementById('desc').innerHTML = "Sorry, we couldn't find the file. Try pasting the link below";
+    }
+  }  
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '',
+      'X-RapidAPI-Key': 'f02d618031msh48b643b8144e0e3p1cf063jsn25d7a2902973',
       'X-RapidAPI-Host': 'youtube-mp3-download1.p.rapidapi.com'
     }
-  };
-  fetch('https://youtube-mp3-download1.p.rapidapi.com/dl?id=PXGycbkbtW0', options)
+  };  
+  fetch(`https://youtube-mp3-download1.p.rapidapi.com/dl?id=${getYouTubeVideoId(pageURL)}`, options)
     .then(response => response.json())
     .then(response => displaylink(response.link))
     .catch(err => console.error(err));
-  
-});
-
+})();
